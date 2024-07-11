8de59a09cffd8b36be92a825f11883efef49b2fd
Ashton <ashton.q.dong@gmail.com>
2024-07-11 13:28:59 -0700

    obstacle spawn

---
+++ Assets/ObstacleSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public GameObject Obstacle;
+    public float SpawnRate = 2;
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < SpawnRate)
+        {
+            timer += Time.deltaTime; 
+        }else
+        {
+            Instantiate(Obstacle, transform.position, transform.rotation);
+            timer = 0;
+        }
+
+    }
+}
