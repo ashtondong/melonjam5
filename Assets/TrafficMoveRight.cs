c111396b3741b2c37784dd27135bad0481556b44
Ashton <ashton.q.dong@gmail.com>
2024-07-19 12:15:06 -0700

    added sample traffic objects and script

---
+++ Assets/TrafficMoveRight.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficMoveRight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3.up * moveSpeed) * Time.deltaTime;
+    }
+}
