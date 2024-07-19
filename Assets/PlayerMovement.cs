52e6f275b71223e6e47a2d6c986636fa98c08ddd
Ashton <ashton.q.dong@gmail.com>
2024-07-19 11:53:44 -0700

    committing update to player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -18,8 +18,10 @@ public class PlayerMovement : MonoBehaviour
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        Vector2 direction = new Vector2(xInput, yInput).normalized;
+        Vector2 direction = new Vector2(xInput, yInput);
 
         body.velocity = direction * speed;
+
+
     }
 }
