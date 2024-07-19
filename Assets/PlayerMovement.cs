56b4868fe1e91b3bb93105ecaa82abe94578d63e
Ashton <ashton.q.dong@gmail.com>
2024-07-19 11:46:30 -0700

    created moveable character

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D body;
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+
+        body.velocity = direction * speed;
     }
 }
