2bcf637d569f099eb7e67767c61a0c319402fb73
Roah Cho <choroah@gmail.com>
2024-07-19 23:13:35 -0400

    created game over screen

---
+++ Assets/GameEnd.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+    
+    public void restartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver() {
+        gameOverScreen.SetActive(true);
+    }
+
+}
